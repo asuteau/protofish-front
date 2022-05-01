eeea3ac6299d0bf612e243a10cb284b5b2208917
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2022-05-01 19:12:45 +0200

    build: set base path based on run mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,46 +5,51 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/protofish-front/",
-  plugins: [
-    vue(),
-    VitePWA({
-      mode: "development",
-      base: "/protofish-front/",
-      srcDir: "src",
-      filename: "service-worker.ts",
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      strategies: "injectManifest",
-      manifest: {
-        name: 'Protofish',
-        short_name: 'Protofish',
-        description: 'Frontend app for Protofish',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ]
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const isDev = mode === 'development'
+  return {
+    // build specific config
+    base: isDev ? "" : "/protofish-front/",
+    plugins: [
+      vue(),
+      VitePWA({
+        mode: "development",
+        base: isDev ? "" : "/protofish-front/",
+        srcDir: "src",
+        filename: "service-worker.ts",
+        includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        strategies: "injectManifest",
+        manifest: {
+          name: 'Protofish',
+          short_name: 'Protofish',
+          description: 'Frontend app for Protofish',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            }
+          ]
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
