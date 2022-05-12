e012b98a6496b07ee08efab59724d91580bca756
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2022-05-12 17:02:38 +0200

    feat: update title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,7 +36,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify 3 Beta
+          Protofish
         </h1>
 
         <h4>Vite Preview</h4>
