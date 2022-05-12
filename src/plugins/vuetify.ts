090b78ba18772787f98382592e5f982cc05895bb
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2022-05-12 14:58:48 +0200

    build: add custom theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,6 +6,20 @@ import '@/styles/variables.scss'
 import { createVuetify } from 'vuetify'
 import { aliases, fa } from 'vuetify/iconsets/fa'
 
+const protofishTheme = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#364F6B',
+    secondary: '#3FC1C9',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  }
+}
+
 export default createVuetify(
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
   {
@@ -14,6 +28,12 @@ export default createVuetify(
       aliases,
       sets: {
         fa,
+      }
+    },
+    theme: {
+      defaultTheme: 'protofishTheme',
+      themes: {
+        protofishTheme,
       }
     }
   }
