576ce7dde8a9bfa28b2437a093f645e2e89acc9f
asuteau <aymeric.suteau.work@gmail.com>
2022-05-13 14:52:35 +0000

    Deploying to gh-pages from @ asuteau/protofish-front@5e290be5874ac64fc5b6ca393f77e0b7318f5898 🚀

---
+++ assets/RankingsView.c7112a82.js
@@ -0,0 +1,1 @@
+import{_ as e,d as n,o as a,c as o}from"./index.bbd30bac.js";const r=n({});function s(t,c,p,_,f,i){return a(),o("h1",null,"Palmar\xE8s")}var d=e(r,[["render",s]]);export{d as default};
---
+++ assets/StatisticsView.610f536e.js
@@ -0,0 +1,1 @@
+import{_ as e,d as t,o as s,c as o}from"./index.bbd30bac.js";const a=t({});function n(c,r,i,p,_,f){return s(),o("h1",null,"Statistiques")}var l=e(a,[["render",n]]);export{l as default};
