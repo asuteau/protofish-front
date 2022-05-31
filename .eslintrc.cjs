ba5f19d85200e78f6c7a280d361f271d5adb3f66
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2022-05-31 16:46:53 +0200

    build: add configuration for Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   "root": true,
   "extends": [
     "eslint:recommended",
+    "prettier",
     "@vue/eslint-config-typescript/recommended",
     "plugin:vue/base", // Settings and rules to enable correct ESLint parsing
     "plugin:vue/vue3-essential", // Plus rules to prevent errors or unintended behavior
